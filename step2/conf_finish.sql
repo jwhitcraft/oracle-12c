c178181b08d1c849b1979cbc3e4a4d60cc2a5705
Jon Whitcraft <jwhitcraft@sugarcrm.com>
2016-08-15 13:26:45 -0400

    more config settings

--- step2/conf_finish.sql
+++ step2/conf_finish.sql
@@ -11,6 +11,13 @@ connect ctxsys/Ctxsys2013
 connect / as sysdba
 spool off
 
+-- make sure the database datafile can auto extend
+alter database datafile '/u01/app/oracle/oradata/ORCL/datafile/o1_mf_system_b1j6hp8b_.dbf' AUTOEXTEND ON;
+
+-- make the default password unlimited life time
+alter profile default limit password_life_time unlimited;
+alter user system identified by manager;
+
 -- Setting Enterprise Manager Express HTTP ports.
 exec dbms_xdb_config.sethttpsport(5500);
 exec dbms_xdb_config.sethttpport(8080);
